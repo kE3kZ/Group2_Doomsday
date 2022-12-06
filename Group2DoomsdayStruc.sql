2e7097b8efc7f647d381699826908b975f8d5ee8
kE3kZ <93687298+kE3kZ@users.noreply.github.com>
2022-12-06 14:49:09 -0600

    Update Group2DoomsdayStruc.sql

--- Group2DoomsdayStruc.sql
+++ Group2DoomsdayStruc.sql
@@ -1,1 +1,48 @@
 
+CREATE DATABASE Doomsday
+GO
+
+USE Doomsday
+
+
+--create Inventory Type Table
+CREATE TABLE InventoryType
+(
+		InventoryTypeID char(4) NOT NULL,
+		InventoryType varchar(50) NOT Null,
+		CONSTRAINT PK_InventoryTypeID PRIMARY KEY (InventoryTypeID)
+);
+
+--create Inventory Table
+CREATE TABLE INVENTORY
+(
+		InventoryID char(4) NOT NULL,
+		CampID char(4) NOT NULL,
+		InventoryTypeID char(4) NOT NULL,
+		Quantity varchar(50) NOT NULL
+		CONSTRAINT PK_InventoryID PRIMARY KEY (InventoryID)
+);
+
+--create Inventory Info Table
+CREATE TABLE InvntoryInfo
+(
+		InventoryID char(4) NOT NULL,
+		InventoryName varchar(50) NOT NULL,
+		InventoryUnit varchar(50) NOT NULL
+		CONSTRAINT PK_InventoryID PRIMARY KEY (InventoryID)
+);
+
+--create Ammo Inventory Table
+CREATE TABLE AmmoInventory
+(
+		CampID char(4) NOT NULL,
+		AmmoID char(4) NOT NULL,
+		InventoryID char(4) NOT NULL
+		CONSTRAINT PK_CampID PRIMARY KEY (CampID)
+);
+
+--create Ammo Table
+CREATE TABLE Ammo
+(
+		
+);
