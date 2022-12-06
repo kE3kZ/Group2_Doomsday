2796a9f0e083aec60c4683b5a225554f9a61f079
kE3kZ <93687298+kE3kZ@users.noreply.github.com>
2022-12-06 15:11:03 -0600

    Update Group2DoomsdayStruc.sql

--- Group2DoomsdayStruc.sql
+++ Group2DoomsdayStruc.sql
@@ -1,4 +1,3 @@
-
 CREATE DATABASE Doomsday
 GO
 
@@ -10,7 +9,8 @@ CREATE TABLE InventoryType
 (
 		InventoryTypeID char(4) NOT NULL,
 		InventoryType varchar(50) NOT Null,
-		CONSTRAINT PK_InventoryTypeID PRIMARY KEY (InventoryTypeID)
+		CONSTRAINT PK_InventoryTypeID PRIMARY KEY (InventoryTypeID),
+		CONSTRAINT FK_InventoryType FOREIGN KEY (InventoryType)
 );
 
 --create Inventory Table
@@ -19,7 +19,7 @@ CREATE TABLE INVENTORY
 		InventoryID char(4) NOT NULL,
 		CampID char(4) NOT NULL,
 		InventoryTypeID char(4) NOT NULL,
-		Quantity varchar(50) NOT NULL
+		Quantity smallint NOT NULL
 		CONSTRAINT PK_InventoryID PRIMARY KEY (InventoryID)
 );
 
@@ -44,5 +44,26 @@ CREATE TABLE AmmoInventory
 --create Ammo Table
 CREATE TABLE Ammo
 (
-		
+		AmmoID char(4) NOT NULL,
+		AmmoName varchar(50) NOT NULL
+		CONSTRAINT PK_AmmoID PRIMARY KEY (AmmoID)
+);
+
+--create Group Type Table
+CREATE TABLE GroupTypeID
+(
+		GroupTypeID char(4) NOT NULL,
+		GroupType varchar(50) NOT NULL
+		CONSTRAINT PK_GroupTypeID PRIMARY KEY (GroupTypeID)
+);
+
+--create GroupsTable
+CREATE TABLE Groups
+(
+		GroupID char(4) NOT NULL,
+		GroupName varchar(50) NOT NULL,
+		GroupTypeID varchar(50) NOT NULL,
+		GroupLeader varchar(50) NOT NULL,
+		GroupDescription varchar(50) NOT NULL
+		CONSTRAINT
 );
