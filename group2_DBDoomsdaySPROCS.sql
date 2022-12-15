b474ece6135d88f428cc55035f4e72467edb474d
dylanhallila <114026793+dylanhallila@users.noreply.github.com>
2022-12-15 13:42:18 -0600

    Add files via upload

---
+++ group2_DBDoomsdaySPROCS.sql
@@ -0,0 +1,111 @@
+CREATE PROCEDURE uspAmmoIncreaseProcedure
+@campid AS CHAR(4),
+@ammoid AS CHAR(4),
+@invid AS CHAR(4),
+@amountammo AS smallint
+AS 
+UPDATE AmmoInventory SET AmmoInventoryQuantity = AmmoInventoryQuantity + @amountammo
+WHERE AmmoID = @ammoid AND InventoryID = @invid AND CampID = @campid;
+
+EXECUTE uspAmmoIncreaseProcedure 'C001', 'A001', 'I001', 30;
+GO
+
+CREATE PROCEDURE uspAmmoDecreaseProcedure
+@campid AS CHAR(4),
+@ammoid AS CHAR(4),
+@invid AS CHAR(4),
+@amountammo AS smallint
+AS 
+UPDATE AmmoInventory SET AmmoInventoryQuantity = AmmoInventoryQuantity - @amountammo
+WHERE AmmoID = @ammoid AND InventoryID = @invid AND CampID = @campid;
+
+EXECUTE uspAmmoDecreaseProcedure 'C001', 'A001', 'I001', 30;
+GO
+
+CREATE PROCEDURE uspInventoryDecreaseProcedure
+@invID AS CHAR(4),
+@campID AS CHAR(4),
+@amountChange AS smallint
+AS 
+UPDATE Inventory SET InventoryQuantity = InventoryQuantity - @amountChange
+WHERE InventoryID = @invID AND CampID = @campID;
+
+EXECUTE uspInventoryDecreaseProcedure 'I001', 'C001', 1;
+GO
+
+CREATE PROCEDURE uspInventoryIncreaseProcedure
+@invID AS CHAR(4),
+@campID AS CHAR(4),
+@amountChange AS smallint
+AS 
+UPDATE Inventory SET InventoryQuantity = InventoryQuantity + @amountChange
+WHERE InventoryID = @invID AND CampID = @campID;
+
+EXECUTE uspInventoryIncreaseProcedure 'I001', 'C001', 1;
+GO
+
+CREATE PROCEDURE uspGroupInventory
+@campOption AS CHAR(1),
+@easyConst AS CHAR(3) = 'C00'
+AS
+SELECT * 
+FROM Inventory
+WHERE CampID = @easyConst + @campOption 
+ORDER BY CampID;
+
+EXECUTE uspGroupInventory '1';
+GO
+
+CREATE PROCEDURE uspSelectAllInCamp
+@campID as CHAR(4)
+AS
+SELECT (PeopleFirstName + ' ' + PeopleLastName) AS PersonFullName, CampLocation
+FROM People
+JOIN PeopleInfo ON People.PeopleID = PeopleInfo.PeopleID
+JOIN Camps ON PeopleInfo.CampID = Camps.CampID
+WHERE PeopleInfo.CampID = @campID;
+
+EXECUTE uspSelectAllInCamp 'C001';
+GO
+
+CREATE PROCEDURE uspEasyIndividualSearch
+@lastname VARCHAR(50)
+AS
+SELECT (PeopleFirstName + ' ' + PeopleLastName) AS FullName
+FROM People
+WHERE PeopleLastName LIKE CONCAT('%', @lastname, '%');
+
+EXECUTE uspEasyIndividualSearch 'Ross';
+GO
+
+CREATE PROCEDURE uspCampInventoryByType
+@campID AS char(4)
+AS
+SELECT Camps.CampID, InventoryType.InventoryType, SUM(InventoryQuantity) AS Quantity
+FROM Camps
+INNER JOIN Inventory ON Camps.CampID = Inventory.CampID
+INNER JOIN InventoryInfo ON Inventory.InventoryID = InventoryInfo.InventoryID
+INNER JOIN InventoryType ON InventoryInfo.InventoryTypeID = InventoryType.InventoryTypeID
+WHERE Camps.CampID = @campID
+GROUP BY Camps.CampID, InventoryType.InventoryType;
+
+EXECUTE uspCampInventoryByType 'C001';
+GO
+
+CREATE PROCEDURE uspCampInventoryTransfer
+@fromcamp AS char(4),
+@tocamp AS char(4),
+@inventoryid AS char(4),
+@amounttransfer AS int
+AS
+IF (SELECT InventoryQuantity FROM Inventory WHERE CampID = @fromcamp AND InventoryID = @inventoryID) >= @amounttransfer
+BEGIN
+	UPDATE Inventory SET InventoryQuantity = InventoryQuantity - @amounttransfer WHERE CampID = @fromcamp AND InventoryID = @inventoryID
+	UPDATE Inventory SET InventoryQuantity = InventoryQuantity + @amounttransfer WHERE CampID = @tocamp AND InventoryID = @inventoryid
+END
+
+ELSE
+	print('Cannot commit transaction, camp of origin does not have enough inventory items to transfer.');
+
+EXECUTE uspCampInventoryTransfer 'C003', 'C002', 'I004', 20;
+GO
