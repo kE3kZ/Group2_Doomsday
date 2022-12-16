4e7e4e94063a88701ae0b821827c000362681e97
dylanhallila <114026793+dylanhallila@users.noreply.github.com>
2022-12-16 00:55:01 -0600

    Update group2_DBDoomsdayQuery.sql

--- group2_DBDoomsdayQuery.sql
+++ group2_DBDoomsdayQuery.sql
@@ -20,3 +20,9 @@ INNER JOIN PeopleSkills ON People.PeopleID = PeopleSkills.PeopleID
 INNER JOIN Skills ON PeopleSkills.SkillID = Skills.SkillID
 ORDER BY SkillName;
 GO
+
+SELECT *
+FROM People
+WHERE PeopleHealth = 0 OR PeopleHealth = 1 OR PeopleHealth = 2
+ORDER BY PeopleFirstname;
+GO
